e33d542c973b0a666f84f32fce1c613b3d9fa265
BAEK SANGHEUN <170410_@naver.com>
2021-04-22 02:14:53 +0900

    # 정규표현식으로 문자열 변경해 멘션 보여주고 컴포넌트 최적화(memo, useMemo)

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -1,15 +1,43 @@
-import React, { VFC } from 'react';
+import React, { VFC, memo, useMemo } from 'react';
 import { ChatWrapper } from './styles';
 import gravatar from 'gravatar';
 import { IDM } from '@typings/db';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link, useParams } from 'react-router-dom';
 
 interface Props {
   data: IDM;
 }
 
 const Chat: VFC<Props> = ({ data }) => {
+  const { workspace } = useParams<{ workspace: string; channel: string }>();
   const user = data.Sender;
+
+  // 컴포넌트 최적화를 위해 값을 기억시키는(캐싱) 함수인 useMemo 함수 사용
+  const result = useMemo(
+    () =>
+      regexifyString({
+        input: data.content,
+        // .은 모든 글자, +는 한 개 이상, ?는 0개나 1개 g는 모두 찾겠다는 의미. d는 숫자. +는 한개 이상 g가 없으면 하나만 찾기. n은 줄바꿈
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+        // id나 줄바꿈을 보면 decorator 함수 내용으로 바꿈
+        decorator(match, index) {
+          const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+          // id만 찾았을 때 매치되는 것이 있으면
+          if (arr) {
+            return (
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+          // 줄바꿈을 찾았으면
+          return <br key={index} />;
+        },
+      }),
+    [data.content], // 해당 값 변경 시 캐싱 풀고 값을 다시 계산
+  );
 
   return (
     <ChatWrapper>
@@ -21,10 +49,10 @@ const Chat: VFC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
 };
 
-export default Chat;
+export default memo(Chat);
